6c6199f09ed8bedfa9060750b687028fc91b6916
pile633 <pile_633@hotmail.com>
2014-10-14 12:47:34 +0200

    Create profile

---
+++ profile.js
@@ -0,0 +1,72 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function Profile(){
+    this._username;
+    this._passwd;
+    this._films = {};
+    this._series = {};
+    this._books = {};
+}
+
+Profile.prototype.getUserName = function()
+{
+    return this._username;
+};
+
+Profile.prototype.getPasswd = function()
+{
+    return this._passwd;
+};
+
+Profile.prototype.setUserName = function(username)
+{
+    this._username = username;
+};
+
+Profile.prototype.setPasswd = function(passwd)
+{
+    this._passwd = passwd;
+};
+
+Profile.prototype.getFilm = function()
+{
+    return this._films;
+};
+
+Profile.prototype.getSerie = function()
+{
+    return this._series;
+};
+
+Profile.prototype.getBook = function()
+{
+    return this._books;
+};
+
+Profile.prototype.setFilm = function(film)
+{
+    this._films = film;
+};
+
+Profile.prototype.setSerie = function(serie)
+{
+    this._series = serie;
+};
+
+Profile.prototype.setBook = function(book)
+{
+    this._books = book;
+};
+
+module.exports = {
+    create: function()
+    {
+        return new Profile();
+    }
+};
+
